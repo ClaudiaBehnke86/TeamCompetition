0423852001ef1c210cbcadf808868782a1bf7853
Claudia Behnke <claudia.behnke@online.de>
2022-06-29 22:14:49 +0200

    display fixed seleciton buttons build

--- JJIFteamComp.py
+++ JJIFteamComp.py
@@ -33,14 +33,14 @@ def calc_overlap(teama, teamb):
 # teams
 teams = ['GER', 'FRA', 'NED', 'USA', 'UAE', 'ISR', 'THA', 'COL']
 
-GER = ["1492", "1446", "1447", "1448", "1456", "1457", "1481"]
-FRA = ["1451", "1447", "1453", "1456", "1457", "1475", "1483"]
-NED = ["1451", "1447", "1448", "1456", "1456", "1457"]
+GER = ["1492", "14511", "1447", "1448", "1456", "1457", "1481"]
+FRA = ["14511", "1447", "1453", "1456", "1457", "1475", "1483"]
+NED = ["14511", "1447", "1448", "1456", "1456", "1457"]
 THA = ["1492", "1453", "1455", "1456", "1481", "1484"]
 USA = ["1475", "1476", "1477", "1483", "1484"]
 ISR = ["1475", "1476", "1477", "1483", "1484"]
 UAE = ["1475", "1476", "1477", "1484"]
-COL = ["1451", "1453", "1477", "1483"]
+COL = ["14511", "1453", "1477", "1483"]
 
 team_dict = {
     'GER' : GER,
@@ -58,8 +58,9 @@ key_map = {
     "1492": "Adults Duo Mixed",
     #"1490": "Adults Duo Women",
     #"1444": "Adults Fighting Men -56 kg",
-    "1451": "Adults Fighting Men -62 kg",
-    "1446": "Adults Fighting Men -69 kg",
+    "14511": "Adults Fighting Men -62 kg & -69 kg",
+    #"1451": "Adults Fighting Men -62 kg",
+    #"1446": "Adults Fighting Men -69 kg",
     "1447": "Adults Fighting Men -77 kg",
     "1448": "Adults Fighting Men -85 kg",
     #"1449": "Adults Fighting Men -94 kg",
@@ -104,6 +105,13 @@ col1, col2 = st.columns(2)
 
 result = calc_overlap(teamA, teamB)
 
+name_list = [key_map.get(item,item) for item in result]
+
+teamA_choice1 = st.sidebar.selectbox('Team A Choice 1', name_list)
+teamB_choice1 = st.sidebar.selectbox('Team B Choice 1', name_list)
+teamA_choice2 = st.sidebar.selectbox('Team A Choice 2', name_list)
+teamB_choice2 = st.sidebar.selectbox('Team B Choice 2', name_list)
+
 good_teamA = len(teamA)/len(result)
 good_teamB = len(teamB)/len(result)
 
@@ -112,33 +120,28 @@ teams_sel = [teamB_name, teamA_name]
  
 values = [good_teamB, good_teamA]
 
-#The plot
-fig = go.Figure(
-    go.Pie(
-    labels = teams_sel,
-    values = values,
-    hoverinfo = "label+percent",
-    textinfo = "value"
-))
+fig1 = go.Figure(go.Bar(
+            x=[good_teamA, good_teamB],
+            y=[teamA_name, teamB_name],
+            marker_color=['red', 'blue'],
+            orientation='h'))
 
 
-
+fig1.update_xaxes(title_text='Chances for good category', range=(0, 1.0))
+fig1.update_yaxes(title_text='Teams')
+st.plotly_chart(fig1)
 
 with col1:
     st.header(teamA_name)
     for i in teamA:
         st.write(key_map[i]) 
-    #st.write(good_teamA)   
 
 with col2:
     st.header(teamB_name)
     for i in teamB:
         st.write(key_map[i]) 
-    #st.write(good_teamB)
 
-st.plotly_chart(fig)
-
-with st.expander("shared category"):
+with st.expander("Shared categories"):
     for i in result:
         st.write(key_map[i]) 
 
@@ -149,11 +152,11 @@ if st.button('Random Category'):
 
     with col1:
         if (randcat in teamA):
-            st.success(str(key_map[randcat]) + 'is in ' + teamA_name)
+            st.success(str(key_map[randcat]) + ' is in ' + teamA_name)
         else:
-            st.error(str(key_map[randcat]) + 'is NOT in ' + teamA_name)
+            st.error(str(key_map[randcat]) + ' is NOT in ' + teamA_name)
     with col2:
         if (randcat in teamB):
-            st.success(str(key_map[randcat]) + 'is in ' + teamB_name)
+            st.success(str(key_map[randcat]) + ' is in ' + teamB_name)
         else:
-            st.error(str(key_map[randcat]) + 'is NOT in ' + teamB_name)
+            st.error(str(key_map[randcat]) + ' is NOT in ' + teamB_name)
