63d876aed77f3fe152fd114891adc882937192c3
Claudia Behnke <claudia.behnke@online.de>
2022-09-20 09:34:59 +0200

    make categories dynamic from sportdata API

--- JJIFteamComp.py
+++ JJIFteamComp.py
@@ -16,21 +16,37 @@ import pandas as pd
 from pandas import json_normalize
 
 
-def getdata(eventid, user, password):
+CLUBNAME_COUNTRY_MAP = {"Belgian Ju-Jitsu Federation": 'BEL',
+                        "Deutscher Ju-Jitsu Verband e.V.": 'GER',
+                        "Federazione Ju Jitsu Italia": 'ITA',
+                        "Romanian Martial Arts Federation": 'ROU'}
+
+def get_athletes_cat(eventid, cat_id, user, password):
     """
     get the athletes form sportdata
     """
 
-    uri = 'https://www.sportdata.org/ju-jitsu/rest/event/members/'+str(eventid)+'/'
+    uri = 'https://www.sportdata.org/ju-jitsu/rest/event/categories/'+str(eventid)+'/'+str(cat_id)+'/'
 
     response = requests.get(uri, auth=HTTPBasicAuth(user, password),)
     d = response.json()
 
-    df_out = json_normalize(d)
-    st.write(df_out)
-    df = df_out[['id', 'first', 'last', 'sex', 'country_code', 'categories']]
+    df_out = json_normalize(d["members"])
 
-
+    if not df_out.empty:
+        if df_out['type'].str.contains('athlete').any():
+            #match to name format of Duo cats
+            df_out['name'] = df_out['first'] + " " + df_out['last']
+            df = df_out[['name','country_code']]
+            df['cat_id'] = str(cat_id)
+        else:
+            # for an unclear reason teams to no have a coutnry code... sigh.. 
+            # i will write a function to convert club name to country... 
+            df_out['country_code'] = df_out['club_name'].replace(CLUBNAME_COUNTRY_MAP)
+            df = df_out[['name', 'country_code']]
+            df['cat_id'] = str(cat_id)
+    else:
+        df =pd.DataFrame()
     return df
 
 
@@ -57,90 +73,100 @@ def intersection(teama, teamb):
     return result_out_overlapp
 
 
-# some variables
-teams = ['DEN','GER', 'FRA', 'NED', 'USA', 'MEX']
-
-DEN = ["FM1", "FM2", "FW1", "FW2", "D"]
-GER = ["FM1", "FM2", "FW2", "JW1", "D"]
-FRA = ["FM2", "FW2", "JM1", "JW1"]
-NED = ["FM1", "FM2", "FW1", "FW2", "JM2", "D"] 
-#THA = ["FM1", "FW1", "FW2", "JM1", "JW1", "JW2", "D"]  # ok
-USA = ["JM2", "FM1", "FW2"]
-#ISR = ["JM1", "JM2", "JW1", "JW2", "D"]  
-UAE = ["FW2", "JM1", "JM2", "JW1", "JW2"]  # ok
-COL = ["FM1", "FW1", "JM2", "JW2"]  # ok
-MEX = ["FM1", "FM2", "FW2", "JM1", "JM2"]
-
-#team_lists = [DEN, GER, FRA, NED, USA, MEX]
-
-#df_list = []
-
-
-#import itertools
-#for a, b in itertools.combinations(team_lists, 2):
-#    df_list.append(len(intersection(a,b)))
-
-#print(df_list) 
-
-#print(df_list)
+#  team categories
+#  taken from outlines https://cdn.sportdata.org/96eb874d-48f8-4ed3-b58a-145b53d43de4/ 
 key_map = { 
-    "FM1": "Adults Fighting Men -62 kg & -69 kg",
-    "FM2": "Adults Fighting Men -77 kg & -85kg",
-    "FW1": "Adults Fighting Women -48 kg & -57 kg",
-    "FW2": "Adults Fighting Women -63 kg & -70 kg",
-    "JM1": "Adults Jiu-Jitsu Men -69 kg & -77 kg",
+    "FM1": "Adults Fighting Men -69 kg",
+    "FM2": "Adults Fighting Men -85kg",
+    "FM3": "Adults Fighting Men +85kg",
+    "FW1": "Adults Fighting Women -52 kg",
+    "FW2": "Adults Fighting Women -63 kg",
+    "FW3": "Adults Fighting Women +63 kg",
+    "JM1": "Adults Jiu-Jitsu Men -69 kg",
     "JM2": "Adults Jiu-Jitsu Men -85 kg",
-    "JW1": "Adults Jiu-Jitsu Women -48 & -57 kg",
+    "JM3": "Adults Jiu-Jitsu Men +85 kg",
+    "JW1": "Adults Jiu-Jitsu Women -52",
     "JW2": "Adults Jiu-Jitsu Women -63 kg",
-    "D": "Adults Duo Mixed",
+    "JW3": "Adults Jiu-Jitsu Women +63 kg",
+    "D": "Adults Duo",
     }
 
-#df = pd.DataFrame(df_lists, columns=key_map.values())
-
-#st.write(df)
-
-#fig = px.density_heatmap(df, x="Team A", y="TEAM B")
-#fig.show()
-
-# map variables to strings
-team_dict = {
-    'DEN': DEN,
-    'GER': GER,
-    'FRA': FRA,
-    'NED': NED,
-    #'THA': THA,
-    'USA': USA,
-    #'ISR': ISR,
-    #'UAE': UAE,
-    #'COL': COL,
-    'MEX': MEX
+# mapping all sportdata cat IDs to that are allowed in event (Adults & U21)
+cat_to_ids = {
+    "FM1": [1444, 1451, 1446, 1429, 1430, 1431],
+    "FM2": [1447, 1448, 1432, 1433],
+    "FM3": [1449, 1450, 1434, 1435],
+    "FW1": [1452, 1453, 1454, 1436, 1437, 1438],
+    "FW2": [1455, 1456, 1439, 1441],
+    "FW3": [1457, 1458, 1442, 1443],
+    "JM1": [1473, 1474, 1475, 1459, 1460, 1461],
+    "JM2": [1476, 1477, 1462, 1463],
+    "JM3": [1478, 1479, 1464, 1465],
+    "JW1": [1480, 1481, 1482, 1466, 1467, 1468],
+    "JW2": [1483, 1484, 1469, 1470],
+    "JW3": [1485, 1486, 1471, 1472],
+    "D": [1491, 1492, 1493, 1487, 1488, 1489],
 }
 
-
 # Main programm starts here
-
-st.title('Team Competition TWG22')
+st.title('Team Competition')
 st.write("Use left hand menue to select the teams")
 
+
 st.sidebar.image("https://i0.wp.com/jjeu.eu/wp-content/uploads/2018/08/jjif-logo-170.png?fit=222%2C160&ssl=1",
-                     use_column_width='always')
+                 use_column_width='always')
+
+
+apidata = st.sidebar.checkbox("Get registration from Sportdata API", 
+                              help="Check if the registration is still open",
+                              value=True)
+if apidata is True:
+    sd_key = st.sidebar.number_input("Enter the number of Sportdata event number",
+                                     help='is the number behind vernr= in the URL', value=325)
+    # create empty temporary list for catgories to merge into team categories
+    list_df_new_total = []
+
+    for x in cat_to_ids.keys(): 
+        ids = cat_to_ids.get(x)
+        list_df_new = []
+        for id_num in ids:
+            athletes_cat = get_athletes_cat(str(sd_key), str(id_num), st.secrets['user'], st.secrets['password'])        
+            list_df_new.append(athletes_cat)
+            df_new = pd.concat(list_df_new)
+            df_new['team_id'] = str(x)
+        list_df_new_total.append(df_new)
+
+df_total = pd.concat(list_df_new_total)
+
+df_teams = df_total[['team_id','name', 'country_code']].groupby(['team_id', 'country_code']).count().reset_index()
+
+
+# add frequency column for counting the number of categories
+df_teams['cat_count'] = df_teams['country_code'].map(df_teams['country_code'].value_counts())
+# only enter teams with at least X categories present
+df_teams = df_teams[df_teams['cat_count'] > 5]
+del df_teams['cat_count']
+
+# allow the selection of teams
+allcountry = df_teams.country_code.unique()
+teams = st.sidebar.multiselect('Select all countries that want to particpate', allcountry, allcountry)
+df_teams = df_teams[df_teams['country_code'].isin(teams)]
 
 # select teams
 teamA_name = st.sidebar.selectbox("1. Team Red",
                                   help="Choose the red team",
                                   options=teams)
-# convert string to variable
-teamA = team_dict[teamA_name]
+
 
 # remove teamA_name from options list
 teamB_name = st.sidebar.selectbox('2. Team Blue',
                                   help="Choose the blue team",
                                   options=[x for x in teams if x != teamA_name])
-teamB = team_dict[teamB_name]
 
-# get the categories as strings
-teamA_str = [key_map[item] for item in teamA]
-teamB_str = [key_map[item] for item in teamB]
+
+# get the categories per team as list
+teamA = df_teams['team_id'][df_teams['country_code'] == teamA_name].tolist()
+teamB = df_teams['team_id'][df_teams['country_code'] == teamB_name].tolist()
 
 # display the teams in app
 col1, col2 = st.columns(2)
@@ -252,10 +278,13 @@ try:
     st.sidebar.write(random)
 except IndexError:
 # create some columns to display the choices
-    if(len(result_over)>1):
+    if(len(result_over)>1 and len(result_over)<3):
         random = st.sidebar.selectbox('Choice 5',
                                  help="Choose category",
                                  options=[x for x in result_st if x not in exclude])
+        st.write(random)
+        st.write("Selection")
+        st.markdown("""---""")
 
     elif(len(result_over)>0):
 
