7b77d921f09f55e4385e1dee03f99b560e366372
Claudia Behnke <claudia.behnke@online.de>
2022-09-20 20:43:11 +0200

    basic functionality

--- JJIFteamComp.py
+++ JJIFteamComp.py
@@ -16,6 +16,8 @@ import json
 import plotly.express as px
 from requests.auth import HTTPBasicAuth
 import pandas as pd 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 from pandas import json_normalize
 
 #  categories and short ids
@@ -145,6 +147,21 @@ def intersection(teama, teamb):
 
     return result_out_overlapp
 
+def rev_look(val, dict):
+    ''' revese lookup of key.
+    Returns first matching key
+    Parameters
+    ----------
+    val
+        value to be looked up
+    dict
+        dict that contains the keys and value
+
+    '''
+    key = next(key for key, value in dict.items() if value == val)
+
+    return key
+
 # Main programm starts here
 st.title('Team Competition')
 st.write("Use left hand menue to select the teams")
@@ -161,28 +178,30 @@ if apidata is True:
     # create empty temporary list for catgories to merge into team categories
     list_df_new_total = []
 
-    for x in TEAMCAT_TO_CATID: 
+    for x in TEAMCAT_TO_CATID:
         ids = TEAMCAT_TO_CATID.get(x)
         list_df_new = []
         for id_num in ids:
-            athletes_cat = get_athletes_cat(str(sd_key), str(id_num), st.secrets['user'], st.secrets['password'])        
+            athletes_cat = get_athletes_cat(str(sd_key),
+                                            str(id_num),
+                                            st.secrets['user'],
+                                            st.secrets['password'])
             list_df_new.append(athletes_cat)
             df_new = pd.concat(list_df_new)
             df_new['team_id'] = str(x)
         list_df_new_total.append(df_new)
-
 df_total = pd.concat(list_df_new_total)
-
 df_teams = df_total[['team_id','name', 'country_code']].groupby(['team_id', 'country_code']).count().reset_index()
 
 
+# remove small teams (small = less than 5 team cat present)
 # add frequency column for counting the number of categories
 df_teams['cat_count'] = df_teams['country_code'].map(df_teams['country_code'].value_counts())
 # only enter teams with at least X categories present
 df_teams = df_teams[df_teams['cat_count'] > 5]
 del df_teams['cat_count']
 
-# allow the selection of teams
+# selection of teams in menue
 allcountry = df_teams.country_code.unique()
 teams = st.sidebar.multiselect('Select all countries that want to particpate', allcountry, allcountry)
 df_teams = df_teams[df_teams['country_code'].isin(teams)]
@@ -192,18 +211,16 @@ teamA_name = st.sidebar.selectbox("1. Team Red",
                                   help="Choose the red team",
                                   options=teams)
 
-
 # remove teamA_name from options list
 teamB_name = st.sidebar.selectbox('2. Team Blue',
                                   help="Choose the blue team",
                                   options=[x for x in teams if x != teamA_name])
 
-
 # get the categories per team as list
 teamA = df_teams['team_id'][df_teams['country_code'] == teamA_name].tolist()
 teamB = df_teams['team_id'][df_teams['country_code'] == teamB_name].tolist()
 
-# display the teams in app
+# display the teams in list
 col1, col2 = st.columns(2)
 with col1:
     st.markdown(f'<p style="color:#F31C2B;font-size:42px;border-radius:2%;">{teamA_name}</p>', unsafe_allow_html=True)
@@ -222,147 +239,257 @@ intersection_teams = intersection(teamA, teamB)
 
 with st.expander("Intersection"):
     st.write("These categories exist in both teams")
-    intersection_teams_st = [TEAMCAT_NAME_DICT.get(item, item) for item in intersection_teams]
-    st.write(intersection_teams_st)
+    intersection_teams_str = [TEAMCAT_NAME_DICT.get(item, item) for item in intersection_teams]
+    st.write(intersection_teams_str)
 
 with st.expander("Selectable categories"):
     st.write("These categories exist in only one of the teams")
     result_st = [TEAMCAT_NAME_DICT.get(item, item) for item in result]
-    result_st_sel = [x for x in result_st if x not in intersection_teams_st]
+    result_st_sel = [x for x in result_st if x not in intersection_teams_str]
     st.write(result_st_sel)
 
+#list to add categories that are already selected
 exclude = []
+# list for selected categories
+selected = []
+# amount of categories that will fight:
+MATCHES = 7
 
-# add overlapping categories in categories list
-try:
-    tA_c1 = intersection_teams_st[0]
-    st.sidebar.write(tA_c1)
-except IndexError:
-    tA_c1 = st.sidebar.selectbox('Choice',
-                             help="Choose category",
-                             options=[x for x in result_st if x not in exclude])
-exclude.append(tA_c1)
 
-try:
-    tB_c1 = intersection_teams_st[1]
-    st.sidebar.write(tB_c1)
-except IndexError:
-    tB_c1 = st.sidebar.selectbox('Choice 2',
+#3 cases:   
+if len(intersection_teams) == MATCHES:
+    # excat the same number
+    # just display the the category and let team select the atlhtes
+
+    st.sidebar.header("Selected categories")
+    st.sidebar.write('- Exact the same categories')
+    st.sidebar.write('Nothing to do')
+    selected = intersection_teams
+    for i in selected:
+        st.sidebar.write(TEAMCAT_NAME_DICT[i])
+
+elif len(intersection_teams) > MATCHES:
+    # more options than MATCHES
+    # remove options
+
+    # remove till even or uneven each team removes same amount of categories
+    # if leftover: random choice
+    st.sidebar.write('- More options than MATCHES')
+    st.sidebar.write('Remove categories')
+    selected = intersection_teams
+
+    #check how many cats are to much:
+    overheadd_cat = len(intersection_teams) - MATCHES
+
+    # remove always 2 categories
+    while overheadd_cat > 2:
+
+        tA = st.sidebar.selectbox('Choice Red',
+                                  help="Choose category to remove",
+                                  options=[TEAMCAT_NAME_DICT[x] for x in selected])
+        # revese lookup of key
+        selected.remove(rev_look(tA, TEAMCAT_NAME_DICT))
+        exclude.append(tA)
+
+        tB = st.sidebar.selectbox('Choice Blue',
+                                  help="Choose category to remove",
+                                  options=[TEAMCAT_NAME_DICT[x] for x in selected])
+        selected.remove(rev_look(tB, TEAMCAT_NAME_DICT))
+        exclude.append(tB)
+
+        overheadd_cat = overheadd_cat - 2
+
+    if (overheadd_cat % 2) == 0:
+        # The remaining number is even
+        st.write("both teams need to choose")
+
+        tA = st.sidebar.selectbox('Choice Red',
+                                  help="Choose category to remove",
+                                  options=[TEAMCAT_NAME_DICT[x] for x in selected])
+        # revese lookup of key
+
+        selected.remove(rev_look(tA, TEAMCAT_NAME_DICT))
+        exclude.append(tA)
+
+        tB = st.sidebar.selectbox('Choice Blue',
+                                  help="Choose category to remove",
+                                  options=[TEAMCAT_NAME_DICT[x] for x in selected])
+        selected.remove(rev_look(tB, TEAMCAT_NAME_DICT))
+        exclude.append(tB)
+
+    else:
+        # the number is odd
+        if st.sidebar.button('Select Random Category to remove',
+                              help="press this button to choose random category to remove"):
+            # random choice from all leftover categories
+            result_over = [x for x in selected]
+            randcat = random.choice(result_over)
+            # display random cat
+            st.sidebar.markdown(f'<p style="background-color:#000000;border-radius:4%;">{TEAMCAT_NAME_DICT[randcat]}</p>', unsafe_allow_html=True)
+            selected.remove(randcat)
+
+    #for i in selected:
+
+    #    st.sidebar.write(TEAMCAT_NAME_DICT[i])
+
+elif len(intersection_teams_str) < MATCHES:
+    # less options than MATCHES
+    # add teams teams
+    st.sidebar.write('Less options than MATCHES')
+    st.sidebar.write('Add categrories')
+    selected = intersection_teams
+    #for i in selected:
+    #    st.sidebar.write(TEAMCAT_NAME_DICT[i])
+
+    # add the selected teams to the excluded list
+    # (they can't be selected twice)
+    exclude.append(intersection_teams_str)
+
+    # check how many teams are missing:
+    miss_cat = MATCHES - len(intersection_teams_str)
+
+    # add always 2 categories
+    while miss_cat > 2:
+
+        tA = st.sidebar.selectbox('Choice Red',
+                                  help="Choose category",
+                                  options=[x for x in result_st_sel if x not in exclude])
+        # revese lookup of key
+        selected.append(rev_look(tA, TEAMCAT_NAME_DICT))
+        exclude.append(tA)
+
+        tB = st.sidebar.selectbox('Choice Blue',
                                  help="Choose category",
-                                 options=[x for x in result_st if x not in exclude])
-exclude.append(tB_c1)
+                                 options=[x for x in result_st_sel if x not in exclude])
+        selected.append(rev_look(tB, TEAMCAT_NAME_DICT))
+        exclude.append(tB)
 
-try:
-    tA_c2 = intersection_teams_st[2]
-    st.sidebar.write(tA_c2)
-except IndexError:
-    tA_c2 = st.sidebar.selectbox('Choice 3',
+        miss_cat = miss_cat - 2
+
+    if (miss_cat % 2) == 0:
+        # The remaining number is even
+        st.write("both teams need to choose")
+
+        tA = st.sidebar.selectbox('Choice Red',
+                                     help="Choose category",
+                                     options=[x for x in result_st_sel if x not in exclude])
+        # revese lookup of key
+        selected.append(rev_look(tA, TEAMCAT_NAME_DICT))
+        exclude.append(tA)
+
+        tB = st.sidebar.selectbox('Choice Blue',
                                  help="Choose category",
-                                 options=[x for x in result_st if x not in exclude])
-exclude.append(tA_c2)
-
-try:
-    tB_c2 = intersection_teams_st[3]
-    st.sidebar.write(tB_c2)
-except IndexError:
-    tB_c2 = st.sidebar.selectbox('Choice 4',
-                                 help="Choose category",
-                                 options=[x for x in result_st if x not in exclude])
-
-exclude.append(tB_c2)
-
-st.header("Selected categories")
-
-st.write(tA_c1)
-st.write("Selection")
-st.markdown("""---""")
-
-st.write(tB_c1)
-st.write("Selection")
-st.markdown("""---""")
+                                 options=[x for x in result_st_sel if x not in exclude])
+        selected.append(rev_look(tB, TEAMCAT_NAME_DICT))
+        exclude.append(tB)
+    else:
+        # the number is odd
+        if st.sidebar.button('Select Random Category',
+                              help="press this button to choose random category"):
+            # random choice from all leftover categories
+            result_over = [x for x in result_st_sel if x not in exclude]
+            randcat = random.choice(result_over)
+            # display random cat
+            st.sidebar.markdown(f'<p style="background-color:#000000;border-radius:4%;">{randcat}</p>', unsafe_allow_html=True)
+            # revese lookup of key
+            key = next(key for key, value in TEAMCAT_NAME_DICT.items() if value == randcat)
+            selected.append(key)
 
 
-st.write(tA_c2)
-st.write("Selection")
-st.markdown("""---""")
+if(len(selected) == MATCHES):
+    st.sidebar.header("Selected categories")
+    for i in selected:
+        st.sidebar.write(TEAMCAT_NAME_DICT[i])
 
-st.write(tB_c2)
-st.write("Selection")
-st.markdown("""---""")
+    st.header("Selected categories - choose athletes")
+
+    col1a, col2a = st.columns(2)
+    with col1a:
+
+        for i, j in enumerate(selected):
+            st.write(TEAMCAT_NAME_DICT[j])
+            st.write(df_total['name'][(df_total['country_code'] == teamA_name) & (df_total['team_id'] == str(j))])
+
+    with col2a:
+        for i, j in enumerate(selected):
+            st.write(TEAMCAT_NAME_DICT[j])
+            st.write(df_total['name'][(df_total['country_code'] == teamB_name) & (df_total['team_id'] == str(j))])
+
 
 
 # with sel1:
-#     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tA_c1}</p>', unsafe_allow_html=True)
+#     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tA}</p>', unsafe_allow_html=True)
 # with sel2:
 #     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tB_c1}</p>', unsafe_allow_html=True)
 # with sel3:
-#     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tA_c2}</p>', unsafe_allow_html=True)
+#     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tB}</p>', unsafe_allow_html=True)
 # with sel4:
 #     st.markdown(f'<p style="background-color:#000000;border-radius:2%;">{tB_c2}</p>', unsafe_allow_html=True)
 
-# variable with all remaining categoies
-result_over = [x for x in result_st if x not in exclude]
-with st.expander("Categories in random draw"):
-    st.write("These categories exist in at least one team and were not selected")
-    st.write(result_over)
+# # variable with all remaining categoies
+# result_over = [x for x in result_st if x not in exclude]
+# with st.expander("Categories in random draw"):
+#     st.write("These categories exist in at least one team and were not selected")
+#     st.write(result_over)
 
-# calculate probabilities
+# # calculate probabilities
 
-try:
-    random = intersection_teams_st[4]
-    st.sidebar.write(random)
-except IndexError:
-# create some columns to display the choices
-    if(len(result_over)>1 and len(result_over)<3):
-        random = st.sidebar.selectbox('Choice 5',
-                                 help="Choose category",
-                                 options=[x for x in result_st if x not in exclude])
-        st.write(random)
-        st.write("Selection")
-        st.markdown("""---""")
+# try:
+#     random = intersection_teams_str[4]
+#     st.sidebar.write(random)
+# except IndexError:
+# # create some columns to display the choices
+#     if(len(result_over)>1 and len(result_over)<3):
+#         random = st.sidebar.selectbox('Choice 5',
+#                                  help="Choose category",
+#                                  options=[x for x in result_st if x not in exclude])
+#         st.write(random)
+#         st.write("Selection")
+#         st.markdown("""---""")
 
-    elif(len(result_over)>0):
+#     elif(len(result_over)>0):
 
-        good_teamA = (len([x for x in teamA_str if x not in exclude])/len(result_over))*100
-        good_teamB = (len([x for x in teamB_str if x not in exclude])/len(result_over))*100
+#         good_teamA = (len([x for x in teamA_str if x not in exclude])/len(result_over))*100
+#         good_teamB = (len([x for x in teamB_str if x not in exclude])/len(result_over))*100
 
 
-         # display probabilties for teams
-        with st.expander("Display probabilties for teams"):
-            teams_sel = [teamB_name, teamA_name]
-            values = [good_teamB, good_teamA]
-            fig1 = go.Figure(go.Bar(
-                         x=[good_teamA, good_teamB],
-                         y=[teamA_name, teamB_name],
-                         text=[teamA_str, teamB_str],
-                         marker_color=['#F31C2B', '#0090CE'],
-                         orientation='h'))
-            fig1.update_xaxes(title_text='Chances for "good" category [%]', range=(0, 100))
-            fig1.update_yaxes(title_text='Teams')
-            st.plotly_chart(fig1)
+#          # display probabilties for teams
+#         with st.expander("Display probabilties for teams"):
+#             teams_sel = [teamB_name, teamA_name]
+#             values = [good_teamB, good_teamA]
+#             fig1 = go.Figure(go.Bar(
+#                          x=[good_teamA, good_teamB],
+#                          y=[teamA_name, teamB_name],
+#                          text=[teamA_str, teamB_str],
+#                          marker_color=['#F31C2B', '#0090CE'],
+#                          orientation='h'))
+#             fig1.update_xaxes(title_text='Chances for "good" category [%]', range=(0, 100))
+#             fig1.update_yaxes(title_text='Teams')
+#             st.plotly_chart(fig1)
 
-        if st.sidebar.button('Select Random Category',
-                              help="press this button to choose random category"):
-             # random choice from all leftover categories
-            randcat = random.choice(result_over)
+#         if st.sidebar.button('Select Random Category',
+#                               help="press this button to choose random category"):
+#              # random choice from all leftover categories
+#             randcat = random.choice(result_over)
         
-            # display random cat
-            st.write(randcat)
-            st.write("Selection")
-            st.markdown("""---""")
+#             # display random cat
+#             st.write(randcat)
+#             st.write("Selection")
+#             st.markdown("""---""")
 
 
-            # show some messages if the category is in
-            with col1:
-                if randcat in teamA_str:
-                    st.success(str(randcat) + ' is in ' + teamA_name)
-                else:
-                    st.error(str(randcat) + ' is NOT in ' + teamA_name)
-            with col2:
-                if randcat in teamB_str:
-                    st.success(str(randcat) + ' is in ' + teamB_name)
-                else:
-                    st.error(str(randcat) + ' is NOT in ' + teamB_name)
+#             # show some messages if the category is in
+#             with col1:
+#                 if randcat in teamA_str:
+#                     st.success(str(randcat) + ' is in ' + teamA_name)
+#                 else:
+#                     st.error(str(randcat) + ' is NOT in ' + teamA_name)
+#             with col2:
+#                 if randcat in teamB_str:
+#                     st.success(str(randcat) + ' is in ' + teamB_name)
+#                 else:
+#                     st.error(str(randcat) + ' is NOT in ' + teamB_name)
+
 
 st.sidebar.markdown('<a href="mailto:sportdirector@jjif.org">Contact for problems</a>', unsafe_allow_html=True)
 
